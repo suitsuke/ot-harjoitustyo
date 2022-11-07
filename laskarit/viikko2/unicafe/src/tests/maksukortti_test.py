1bba9fcdac6ccf970c31ae42c8b47b31404656be
Tia Toppari <suicuazul@gmail.com>
2022-11-07 11:02:19 +0200

    tehtävä 6

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,24 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    
+    def test_alkusaldo_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_lataus_toimii(self):
+        self.maksukortti.lataa_rahaa(100)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 11.00 euroa")
+
+    def test_rahan_otto_tarpeeksi_rahaa(self):
+        self.maksukortti.ota_rahaa(800)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 2.00 euroa")
+    
+    def test_rahan_otto_ei_tarpeeksi_rahaa(self):
+        self.maksukortti.ota_rahaa(1200)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_metodi_palauttaa_true_jos_onnistui(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(800), True)
+    
+    def test_metodi_palauttaa_false_jos_epaonnistui(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1200), False)
