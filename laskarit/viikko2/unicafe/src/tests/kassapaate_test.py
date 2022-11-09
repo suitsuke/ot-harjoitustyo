7ea727799c5477425b045a6c7bf1abb6a78a339a
Tia Toppari <suicuazul@gmail.com>
2022-11-09 13:22:29 +0200

    t8 valmis

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -1,10 +1,67 @@
 import unittest
-#from maksukortti import Maksukortti
+from maksukortti import Maksukortti
 from kassapaate import Kassapaate
 
-class TestKassapaate(unittest.Testcase):
+class TestKassapaate(unittest.TestCase):
     def setUp(self):
         self.kassapaate = Kassapaate()
     
     def test_alkuraha_oikein(self):
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_alkulounaita_edulliset_myyty(self):
+        self.assertEqual(self.kassapaate.edulliset, 0)
+    
+    def test_alkulounaita_maukkaat_myyty(self):
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+
+    def test_kateisosto_edullinen_riittava_maksu(self):
+        self.assertEqual(self.kassapaate.syo_edullisesti_kateisella(250), 10)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100240)
+        self.assertEqual(self.kassapaate.edulliset, 1)
+    
+    def test_kateisosto_maukas_riittava_maksu(self):
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kateisella(450), 50)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100400)
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+    
+    def test_kateisosto_edullinen_ei_riittava_maksu(self):
+        self.assertEqual(self.kassapaate.syo_edullisesti_kateisella(220), 220)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+        self.assertEqual(self.kassapaate.edulliset, 0)
+    
+    def test_kateisosto_maukas_ei_riittava_maksu(self):
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kateisella(390), 390)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+    
+    def test_korttiosto_edullinen_riittavasti_rahaa(self):
+        maksukortti = Maksukortti(1000)
+        self.assertEqual(self.kassapaate.syo_edullisesti_kortilla(maksukortti), True)
+        self.assertEqual(self.kassapaate.edulliset, 1)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+    
+    def test_korttiosto_maukas_riittavasti_rahaa(self):
+        maksukortti = Maksukortti(1000)
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kortilla(maksukortti), True)
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_korttiosto_edullinen_ei_riittavasti_rahaa(self):
+        maksukortti = Maksukortti(100)
+        self.assertEqual(self.kassapaate.syo_edullisesti_kortilla(maksukortti), False)
+        self.assertEqual(self.kassapaate.edulliset, 0)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_korttiosto_maukas_ei_riittavasti_rahaa(self):
+        maksukortti = Maksukortti(100)
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kortilla(maksukortti), False)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+    
+    def test_lataa_kortti(self):
+        maksukortti = Maksukortti(100)
+        self.kassapaate.lataa_rahaa_kortille(maksukortti, 1000)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 101000)
+        self.assertEqual(maksukortti.saldo, 1100) 
+
