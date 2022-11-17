2e57ecd6534a22f9c8aed576f8508a2100f44a3b
Tia Toppari <suicuazul@gmail.com>
2022-11-17 11:16:38 +0200

    first database testing working

--- src/repositories/stickers_repository.py
+++ src/repositories/stickers_repository.py
@@ -5,9 +5,8 @@ class StickersRepository:
     #controls UserStickers, i.e. who has which stickers
     # (stickers.db should not be modified at any poin by the app)
 
-    def __init__(self, file_path):
-        self.db = sqlite3.connect(file_path)
-        self.db.isolation_level = None
+    def __init__(self, database):
+        self.db = database
 
     def find_by_user(self, username):
         #finds a list of stickers aqcuired by user and returns their id:s in a list
@@ -22,11 +21,12 @@ class StickersRepository:
         self.db.execute("INSERT INTO UserStickers (user_id, sticker_id) VALUES (?, ?)", [user_id, sticker_id])
         
         #returns what was just put in, in a
-        id = self.db.execute("SELECT UserStickers.user_id, UserStickers.sticker_id from UserStickers WHERE UserStickers.user_id=? AND UserSticker.sticker_id=?", [user_id, sticker_id]).fetchone()    
+        id = self.db.execute("SELECT user_id, sticker_id from UserStickers WHERE user_id=? AND sticker_id=?", [user_id, sticker_id]).fetchone()    
         return id
 
     def remove_sticker(self, username, sticker):
         #removes sticker ownership from a user (if they own it)
+        #return true if successful, false if not
         pass
 
     def check_ownership(self, username, sticker):
--- src/tests/stickersrepository_test.py
+++ src/tests/stickersrepository_test.py
@@ -5,15 +5,29 @@ from repositories.stickers_repository import StickersRepository
 
 class TestStickersRepository(unittest.TestCase):
     def setUp(self):
-        os.remove("test_userstickers.db")
-        db = sqlite3.connect("src/tests/test_userstickers.db")
-        db.isolation_level = None
-        db.execute("CREATE TABLE UserStickers (user_id INTEGER REFERENCES Users, sticker_id INTEGER REFERENCES Stickers)")
-        self.db = StickersRepository("test_userstickers.db")
+        os.remove("src/tests/userstickers.db")
+        testdb = sqlite3.connect("src/tests/userstickers.db")
+        testdb.isolation_level = None
+        #testdb.execute("CREATE TABLE UserStickers (user_id INTEGER REFERENCES Users, sticker_id INTEGER REFERENCES Stickers)")
+        testdb.execute("CREATE TABLE UserStickers (user_id, sticker_id)")
+        testdb.execute("INSERT INTO UserStickers (user_id, sticker_id) VALUES (0, 0)")
+        self.testdb =testdb
+        self.repo = StickersRepository(self.testdb)
+        
 
+    def test_testinit(self):
+        zero = self.testdb.execute("SELECT * FROM UserStickers").fetchone()
+        self.assertEqual(zero, (0,0))
 
     def test_add_sticker(self):
-        self.assertEqual(0,0)   #test for tests
-        self.db
-        #i=self.db.add_sticker(2,3)
-        self.assertEqual(i, (2,3))
+        #self.assertEqual(0,0)   #test for tests
+        #self.db
+        self.assertEqual(self.repo.add_sticker(2,3), (2,3))
+        self.assertEqual(self.repo.add_sticker(1,1), (1,1))
+
+    def test_remove_sticker(self):
+        self.repo.add_sticker(1,1)
+        self.repo.add_sticker(2,3)
+        return
+        self.assertEqual(self.repo.remove_sticker(2,3), True) #returns true if sticker was removed
+        self.assertEqual(self.repo.remove_sticker(2,5), False) #returns false if nothing was removed
